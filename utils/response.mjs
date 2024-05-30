c8f0a6e04c42493261ce930100f55680cc1e7659
Sami <hellosami@protonmail.com>
2024-05-30 16:03:06 +0200

    Add http response

---
+++ utils/response.mjs
@@ -0,0 +1,9 @@
+export function sendResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+}
