cfd832d98e9a7e3e5249b80630de7862e4c6c157
Sami <hellosami@protonmail.com>
2024-05-30 13:30:26 +0200

    Fix database utils

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,4 +1,9 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-const client = new DynamoDB();
+const client = new DynamoDB({
+  region: "eu-north-1",
+});
+
 const db = DynamoDBDocument.from(client);
+
+export { db };
