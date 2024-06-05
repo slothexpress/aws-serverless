c098fa19103f8b145acb1ec77f811137f3cd9232
Sami <hellosami@protonmail.com>
2024-06-05 12:02:11 +0200

    Replace sample code for Delete function

--- functions/todo/delete.mjs
+++ functions/todo/delete.mjs
@@ -1,8 +1,35 @@
 import { db } from "../../utils/db.mjs";
+import { response } from "../../utils/response.mjs";
 
 export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: "DELETE " + event.pathParameters.id,
-  };
+  const { id } = event.pathParameters;
+
+  try {
+    const data = await db.get({
+      TableName: "todos",
+      Key: {
+        pk: "TODO",
+        sk: id,
+      },
+    });
+
+    if (!data.Item) {
+      return response(404, {
+        error: "Nothing to delete. TODO not found for id: " + id,
+      });
+    }
+
+    await db.delete({
+      TableName: "todos",
+      Key: {
+        pk: "TODO",
+        sk: id,
+      },
+    });
+
+    return response(200, { message: "Deleted TODO with id: " + id });
+  } catch (error) {
+    console.error("Error deleting TODO from DynamoDB", error);
+    return response(500, { error: "Could not delete TODO" });
+  }
 };
