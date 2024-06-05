3c29140e50121aac978ad8ad8e673c479ab558bc
Sami <hellosami@protonmail.com>
2024-06-05 11:54:27 +0200

    Replace sample code for Get function

--- functions/todo/get.mjs
+++ functions/todo/get.mjs
@@ -4,10 +4,22 @@ import { response } from "../../utils/response.mjs";
 export const handler = async (event) => {
   const { id } = event.pathParameters;
 
-  const sampleTodo = {
-    id: id,
-    task: "Sami Shower!",
-  };
+  try {
+    const data = await db.get({
+      TableName: "todos",
+      Key: {
+        pk: "TODO",
+        sk: id,
+      },
+    });
 
-  return response(200, sampleTodo);
+    if (!data.Item) {
+      return response(404, { error: "TODO not found for id: " + id });
+    }
+
+    return response(200, data.Item);
+  } catch (error) {
+    console.error("Error retrieving TODO from DynamoDB", error);
+    return response(500, { error: "Could not retrieve TODO" });
+  }
 };
