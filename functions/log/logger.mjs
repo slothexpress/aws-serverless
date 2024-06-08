e745ecb56cb48eb6d5826030c748ef14b2371e35
Sami <hellosami@protonmail.com>
2024-06-08 13:29:15 +0200

    EventBridge routes event to logger

--- functions/log/logger.mjs
+++ functions/log/logger.mjs
@@ -1,11 +1,8 @@
 export const handler = async (event) => {
-  console.log("Samis event received:", JSON.stringify(event, null, 2));
-
-  for (const record of event.Records) {
-    const detail = JSON.parse(record.body);
-    console.log("Detail:", detail);
-  }
-
+  console.log("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ");
+  console.log("Samis event received:");
+  console.log(event);
+  console.log("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ");
   return {
     statusCode: 200,
     body: JSON.stringify("Sami event processed."),
--- functions/todo/create.mjs
+++ functions/todo/create.mjs
@@ -1,5 +1,8 @@
 import { db } from "../../utils/db.mjs";
 import { v4 as uuidv4 } from "uuid";
+import { EventBridge } from "@aws-sdk/client-eventbridge";
+
+const eventBridge = new EventBridge({ region: "eu-north-1" });
 
 export const handler = async (event) => {
   try {
@@ -25,6 +28,24 @@ export const handler = async (event) => {
 
     await db.put(params);
 
+    console.log("SAMI CREATE TODO OK");
+
+    const eventParams = {
+      Entries: [
+        {
+          Source: "todo.put",
+          EventBusName:
+            "arn:aws:events:eu-north-1:655905758223:event-bus/default",
+          DetailType: "TODO Created",
+          Detail: JSON.stringify({ id, task }),
+        },
+      ],
+    };
+
+    console.log("SAMI RAISE EVENT OK");
+
+    await eventBridge.putEvents(eventParams);
+
     return {
       statusCode: 201,
       body: JSON.stringify({
@@ -36,7 +57,7 @@ export const handler = async (event) => {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: "Failed to create TODO:" + task,
+        message: "Failed to create TODO: " + task,
         error: error.message,
       }),
     };
